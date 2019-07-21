57c4143d6bc7f9c20a284db27b4dddef1188d09c
shinmaibam <shinmaibam@192.168.0.103>
2019-07-21 16:26:07 +0530

    updated Hana1

--- src/Hana/Hana1.java
+++ src/Hana/Hana1.java
@@ -3,9 +3,14 @@ package Hana;
 public class Hana1 {
 
 	public static void main(String[] args) {
-
+		int count = 0;
 		for(int i=0; i<10; i++) {
 		System.out.println("This is a test project >> " + i);
+		count ++;
+		if(count >=5) {
+			System.out.println("Breaking the for loop at: "+ i);
+			break;
+		}
 		}
 	}
 
